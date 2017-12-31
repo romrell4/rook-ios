71a577b89a1d63f2155360d37cf9b0b32554d666
Eric Romrell <eric_romrell@byu.edu>
2017-12-31 15:56:55 -0700

    Added cooapods

---
+++ Rook/Rook/support/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Rook
+//
+//  Created by Eric Romrell on 12/29/17.
+//  Copyright © 2017 Eric Romrell. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		FirebaseApp.configure()
+		return true
+	}
+}
+
